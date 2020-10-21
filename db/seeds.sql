677d96df97c67e743768cd3e75d0c876f5fde4d7
ryan hatfield <hatfield086@gmail.com>
2020-10-20 22:53:38 -0500

    db folder and file setup w/ sql files, config folder w/ connection.js

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+("Ultimate Bacon Cheeseburger", false),
+("Mushroom Swiss Burger", false),
+("Hatch Green Chile Bacon Burger", false),
+("Avocado Bacon Burge", false)
